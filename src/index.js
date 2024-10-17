ceaf79eb9c2c25ee4b3d382e4f6ff6239810e242
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-17 17:18:26 +0530

    fix: cors syntax error fix in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 
-app.cors();
+app.use(cors());
 app.use(express.json());
 
 
