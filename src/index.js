697826b81013081bcbd76566fa0a5878f228ebae
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-18 10:54:23 +0530

    fix: add .sequelizerc file for configuring id pass of db from env for migrations

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ const { sequelize } = require('./models');
 const process = require('process');
 
 const app = express();
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 
 sequelize.authenticate();
 app.use(cors());
 app.use(express.json());
 
-
 //health-check
 app.get('/health', (req, res) => {
-	res.send({message: "Health ok!"})
-})
-
+	res.send({ message: 'Health ok!' });
+});
 
 app.listen(PORT, () => {
 	console.log(`server is running on ${PORT}`);
